1f6cf98e069f63d8b3c8f11395b3db0cb786a7a8
Juan Pablo <juanpablom6789@gmail.com>
2023-10-14 22:38:33 -0300

    Agregado little data gen

---
+++ Little_Data/Little_Data_Gen_lcm-sigma.jl
@@ -0,0 +1,94 @@
+include("Gen_Read_Data-Hahn.jl")
+using CSV
+using DataFrames
+
+# Arhivo que utiliza Gen_Read_Data-Hahn.jl para generar datos de señales de Hahn y distribuciones de tamaños de compartimientos.
+# Es necesario tener una gilla de datos variando los parámetros de entrada, es decir variando:
+# la longitud de correlación media lcm y la desviación estándar σ, para generar las distribuciones de tamaños de compartimientos
+# el resto de parametros se mantienen fijos
+
+#------------------------------------------------------------------------------------------
+# Parámetros fijos
+
+# Lo que dejamos constante es el número de compartimientos, el rango de tamaños de correlación lc, el tiempo de simulación final y el muestreo de timepos
+N = 2000
+time_sample_lenght = 200
+
+# Rango de tamaños de compartimientos en μm
+l0 = 0.01
+lf = 15
+
+# Tiempo final de simulación en s
+tf = 1
+
+# Ahora generamos los datos para eso necesitamos hacer el sampling de los lc y los t
+lc = range(l0, lf, length = N)
+t = range(0, tf, length = time_sample_lenght)
+
+# Parametros que se varian
+
+# Rango de tamaños medios de correlación en μm
+lcms = 0.5:0.01:6
+σs = 0.01:0.05:1
+
+#------------------------------------------------------------------------------------------
+# Generación de datos en CSV para cada combinación de parámetros en el path especificado, este va a ser el mismo que use para leer los datos
+path = "C:/Users/Propietario/Desktop/ib/5-Maestría/GenData-PCA-UMAP/Little_Data/Little_Data_CSV"
+
+Gen_Read_Data.GenerarDatosCSV(N, time_sample_lenght, l0, lf, tf, lcms, σs, path)
+
+#------------------------------------------------------------------------------------------
+
+# Se pueden leer los datos generados y guardarlos de manera mas comprimida quendandose solo con las señales y las distribuciones
+
+# Lectura de los datos que se generaron, devuelve un array de 3 dimensiones con los datos de las señales y las distribuciones especificado cada lcm y σ
+
+Probabilitys, Signals = ReadCSVData(N, time_sample_lenght, l0, lf, tf)
+
+# Para esto tenemos que hacer un reshape de los datos para que queden en un arreglo de 2 dimensiones
+
+lenght_σs =  length(σs)
+lenght_lcms = length(lcms)
+max_lenght = maximum(length.([t, lc]))
+
+
+function reshape_data(old_matrix, old_shape, new_shape)
+
+    old_matrix = old_matrix
+
+    dim1, dim2, dim3 = old_shape
+
+    new_matrix = zeros(Float64, dim3, dim1*dim2)
+
+    for i in 1:dim1
+        for j in 1:dim2
+            for k in 1:dim3
+                new_matrix[k,(i - 1) * dim2 + j] = old_matrix[i,j,k]
+            end
+        end
+    end
+
+    return new_matrix
+
+end
+
+# Nuevo tamaño de los datos
+new_size = (lenght_σs * lenght_lcms, max_lenght)
+
+# Ahora si tenemos los datos de entrada y salida es decir las señales y las distribuciones de probabilidad
+dataSignals = reshape_data(Signals, size(Signals), new_size)
+dataProbd = reshape_data(Probabilitys, size(Probabilitys), new_size)
+
+# En un momento para tener un DataFrame llenamos los datos de la señal con 0s los sacamos de cada columna.
+dataIN = dataIN[1:length_t, :]
+
+# Ahora podemos guardar estos datos para hacer un pre procesamiento antes de utilizarlos como entrada de una red neuronal
+
+# Elegir el path donde se van a guardar los datos
+path_save = "C:\\Users\\Propietario\\Desktop\\ib\\5-Maestría\\GenData-PCA-UMAP\\Little_Data\\Little_Data_CSV"
+
+df_dataSignals = DataFrame(dataSignals, :auto)
+df_dataProbd = DataFrame(dataProbd, :auto)
+
+CSV.write(path_save * "\\dataSignals.csv", df_dataSignals)
+CSV.write(path_save * "\\dataOUT.csv", df_dataProbd)
